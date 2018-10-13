e4cf6f2d5e06505e073738850208f1e35cc4f6ec
Jaiaid <jaiaidmobin@gmail.com>
2018-10-14 00:33:07 +0600

    added multi threading code. code is tested

--- log_reg_case.cpp
+++ log_reg_case.cpp
@@ -1,4 +1,6 @@
 #include <cstring>
+#include <pthread.h>
+#include <semaphore.h>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -16,17 +18,53 @@
 #define CLASS_NAME1 "Case"
 #define CLASS_NAME2 "Control"
 #define CHUNK_SIZE 10000
-//#define DEBUG
+// #define DEBUG
+
 
 using namespace std;
+
+
+// variables, semaphores and mutex used to sync and control thread life
+pthread_mutex_t done_count_lock;
+int done_count;
+int thread_exit_signal;
+sem_t all_start;
+sem_t all_done;
 
 ifstream con_file;
 ifstream feature_z_file;
 ifstream ind_file;
 ifstream total_file;
 
+
+// These variables are global to ease the passing to multiple threads
+int start_indx;
+int num_of_thread;
+int read_row_count;
+
+int mx_iter;
+double learn_rate;
+
+std::vector<std::vector<double> > Z;
+std::vector<double> Y;
+std::vector<unsigned long long int> totals;
+std::vector<std::vector<unsigned long long int> > kmercounts;
+std::vector<double> output;
+std::vector<std::vector<double> > global_features_NULL;
+std::vector<std::vector<double> > global_features_ALT;
+std::vector<double> null_model;
+
+
+struct thread_info
+{
+	int thread_no;
+};
+
+
 int open_file_connection();
 int find_row_count();
+void init_sync_primitve();
+void * worker_thread_func(void *thread_no_as_ptr);
 
 void printmodel(vector<double>model){
     for(size_t i = 0; i<model.size(); ++i){
@@ -35,30 +73,29 @@ void printmodel(vector<double>model){
     cout<<"\n";
 }
 
-int main()
+int main(int argc,char **argv)
 {
+	num_of_thread = 1;
+	if(argc==3 && strcmp(argv[1],"-p")==0) {
+		num_of_thread = atoi(argv[2]);
+	}
+	
     if(open_file_connection()) {
         cout<<"Error in opening file"<<std::endl;
         return 0;
     }
 
-    double learn_rate = 0.1; 
-    int mx_iter = 25;
-    //cin>>learn_rate>>mx_iter;
+    learn_rate = 0.1; 
+    mx_iter = 25;
 
     unsigned int nrow = find_row_count();
 
 	//Y.size() and nrow are equal
-    std::vector<std::vector<double> > Z(nrow,std::vector<double>(PCA_COUNT,0));
-    std::vector<double> Y(nrow);
-    std::vector<unsigned long long int> totals(nrow);
-    std::vector<double> counts(Y.size());
-	std::vector<std::vector<unsigned long long int> > kmercounts;
-
-	/*
-	 * 51-82 does 10,17 and 22-36 of R script
-	 */
-    for(unsigned int l=0;l<Y.size();l++)
+    Z = std::vector<std::vector<double> >(nrow,std::vector<double>(PCA_COUNT,0));
+    Y = std::vector<double>(nrow);
+    totals = std::vector<unsigned long long int>(nrow);
+    
+	for(unsigned int l=0;l<Y.size();l++)
     {
         char buf[MAX_LINE_LENGTH];
         char class_name[CLASS_NAME_LENGTH];
@@ -128,33 +165,47 @@ int main()
 	 */
 	int chunk_size = CHUNK_SIZE;
 	
-	std::vector<std::vector<double> > features_NULL(nrow,std::vector<double>(NULL_MODEL_FEATURE_COUNT+1));
-	std::vector<std::vector<double> > features_ALT(nrow,std::vector<double>(ALT_MODEL_FEATURE_COUNT+1));
+	global_features_NULL = std::vector<std::vector<double> >(nrow,std::vector<double>(NULL_MODEL_FEATURE_COUNT+1));
+	global_features_ALT = std::vector<std::vector<double> >(nrow,std::vector<double>(ALT_MODEL_FEATURE_COUNT+1));
 
 	for(unsigned int l=0;l<nrow;l++)
 	{
-		features_NULL[l][0] = 1;
-		features_NULL[l][1] = Z[l][0];
-		features_NULL[l][2] = Z[l][1];
-		features_NULL[l][3] = totals[l];
+		global_features_NULL[l][0] = 1;
+		global_features_NULL[l][1] = Z[l][0];
+		global_features_NULL[l][2] = Z[l][1];
+		global_features_NULL[l][3] = totals[l];
 	}
 
 	for(unsigned int l=0;l<nrow;l++)
 	{
-		features_ALT[l][0] = 1;
-		features_ALT[l][1] = Z[l][0];
-		features_ALT[l][2] = Z[l][1];
-		features_ALT[l][3] = totals[l];
+		global_features_ALT[l][0] = 1;
+		global_features_ALT[l][1] = Z[l][0];
+		global_features_ALT[l][2] = Z[l][1];
+		global_features_ALT[l][3] = totals[l];
 	}
 
-	std::vector<double>null_model = glm(features_NULL,Y,0.1,20);
-	//printmodel(null_model);
-    int chunkread = 0;
-    int data = 1;
-	while(true)
+	null_model = glm(global_features_NULL,Y,0.1,20);
+
+	output = std::vector<double>(CHUNK_SIZE);
+	int chunkread = 0;
+    
+    /*
+     * First we will init sync primitive to create a signal for threads 
+     * to start
+     */
+    init_sync_primitve();
+    // Then create the thread(s)
+    std::vector<pthread_t> thread_list(num_of_thread);
+    for(int l=0;l<num_of_thread;l++)
+    {
+		thread_info *info = new thread_info;
+		info->thread_no = l;
+		pthread_create(&thread_list[l], NULL, worker_thread_func, (void *)info);
+	}
+
+    while(true)
     {
         char buf[MAX_LINE_LENGTH];
-        int read_row_count;
         kmercounts.clear();
         for(read_row_count=0;read_row_count<chunk_size;read_row_count++)
         {
@@ -170,6 +221,7 @@ int main()
 				con_file>>kmercounts[read_row_count][l];
 			}
 		}
+
 		//kmercounts er size protibar CHUNK_SIZE kore bartese
 		//kmercounts er each row te 15 ta column
 
@@ -187,93 +239,53 @@ int main()
 			cout<<std::endl;
 		}
         #endif
-        //cout<<read_row_count<<std::endl;
+        //read_row_count is equal to CHUNK_SIZE
+        //shudhu sesh bar ektu kom hoite pare
         /*
-         * below loop is R script's %dopar% loop
-         * this loop's each itration will be divided to different threads (possibly)
-         * output of each iteration are arranged in a row with column count==read_row_count
-         */
-         //read_row_count is equal to CHUNK_SIZE
-         //shudhu sesh bar ektu kom hoite pare
-        for(int l=0;l<read_row_count;l++)
-        {
-			for(unsigned int l1=0;l1<Y.size();l1++)
-			{
-				counts[l1] = kmercounts[l][l1]/(double)totals[l1];
-				//cout<<counts[l1]<<" ";
-			}
-			//cout<<"\n";
-			//create the fourth column of matrix
-			for(unsigned int l1=0;l1<Y.size();l1++)
-			{
-				features_ALT[l1][4] = counts[l1];
-			}
-			//glm and anova will be here (possibly)
-			
-            std::vector<double> alt_model = glm(features_ALT,Y,learn_rate,mx_iter);
-            
-            //printmodel(alt_model);
-			
-            double alt_likelihood = 1.0;
-			for(int dat = 0; dat < features_ALT.size(); ++dat){
-				std::vector<double> data(features_ALT[0].size());
-				for(int j = 0; j<features_ALT[0].size(); ++j){
-					data[j] = features_ALT[dat][j];
-				}
-				double p = predict(alt_model, data);
-				//cout<<p<<"\n";
-				if( Y[dat] == 1){
-					alt_likelihood = alt_likelihood*p;
-				}
-				else{
-					alt_likelihood = alt_likelihood*(1.0-p); 
-				}
-			}
+         * Below for loop can be done in parallel. The plan is to divide the  
+         * available iteration to multiple thread. If one thread is complete 
+         * it will increase a signal variable from sequence of signal and go to sleep. 
+         * If all signals are marked than main thread will write the result in stdout then  
+         * read more data and signal the worker thread to restart. Main thread will act 
+         * as a watcher.
+         * 
+         * Every thread will do interleaved reading from kmercounts and write to 
+         * particular loaction exclusive to thread. As it's write operation is not in 
+         * same memory address for different thread no synchrnization needed
+         */ 
+        
+        pthread_mutex_lock(&done_count_lock);
+        /* 
+         * done_count holds how many thread has completed their part
+         * done_count is 0. So that, last thread to complete this iteration 
+         * can read the done_count and signal main thread appropriately
+		 */
+		done_count = 0;
+		pthread_mutex_unlock(&done_count_lock);
 
-			//calculating null likelihood
-			//cout<<"null_likelihood calculation\n";
-			double null_likelihood = 1.0;
-			for(int dat = 0; dat < features_NULL.size(); ++dat){
-				std::vector<double>data(features_NULL[0].size());
-				for(int j = 0; j<features_NULL[0].size(); ++j){
-					data[j] = features_NULL[dat][j];
-				}
-				double p = predict(null_model, data);
-
-				if( ((int)Y[dat]) == 1){
-					null_likelihood = null_likelihood*p;
-				}
-				else{
-					null_likelihood = null_likelihood*(1.0-p); 
-				}
-			}
-
-            //cout<<null_likelihood<<" "<<alt_likelihood<<"\n";
-
-
-			if((null_likelihood)==0 && (alt_likelihood)==0.0) {
-				null_likelihood = 0.001;
-				alt_likelihood = 1.0;
-			}
-
-            double likelihood_ratio = null_likelihood/alt_likelihood;
-            double log_likelihood_ratio = -2.0*(log(likelihood_ratio));
-
-            //cout<<log_likelihood_ratio<<"\n";
-
-            if(fabs(log_likelihood_ratio)<eps || log_likelihood_ratio<0.0){
-                log_likelihood_ratio = 0.0;
-            }
-
-            cout<<alglib::chisquarecdistribution(1, log_likelihood_ratio)<<"\n";
-            
-            data++;
-			
+		sem_init(&all_done, 0, 0);
+		// sem_init(&all_start, 0, num_of_thread-1);
+		for(int l=0;l<num_of_thread;l++) sem_post(&all_start);
+        
+        sem_wait(&all_done);
+		// all_done semphore is posted, means all outputs are prepared. 
+		// So, dump them to stdout
+		
+		for(int l=0;l<read_row_count;l++)
+		{
+			cout<<output[l]<<endl;
 		}
-        //cout<<read_row_count<<std::endl;
+		//cout<<"write"<<endl;
+		cout.flush();
+		//cout<<"flush"<<endl;
+		
         if(read_row_count < chunk_size) {
+			thread_exit_signal = 1;
+			for(int l=0;l<num_of_thread;l++) sem_post(&all_start);
 			break;
 		}
+		start_indx += read_row_count;
+
     }
 
     return 0;
@@ -306,6 +318,7 @@ int open_file_connection()
     return 0;
 }
 
+
 int find_row_count()
 {
     int nrow = 0;
@@ -321,4 +334,102 @@ int find_row_count()
     total_file.seekg(0,ios::beg);
 
 	return nrow;
+}
+
+
+void init_sync_primitve()
+{
+	sem_init(&all_start, 0, 0);
+	sem_init(&all_done, 0, 0);
+	pthread_mutex_init(&done_count_lock, 0);
+	thread_exit_signal = 0;
+}
+
+
+void * worker_thread_func(void *arg)
+{
+	int thread_no = ((thread_info *)arg)->thread_no;
+	int interleave = num_of_thread;
+	
+	std::vector<double> counts(Y.size());
+	std::vector<std::vector<double> > thread_local_features_ALT(global_features_ALT);
+	
+	while(true)
+	{
+		sem_wait(&all_start);
+		if(thread_exit_signal==1) {
+			break;
+		}
+		
+		for(int l=thread_no;l<read_row_count;l+=interleave)
+		{
+			for(unsigned int l1=0;l1<Y.size();l1++)
+			{
+				counts[l1] = kmercounts[l][l1]/(double)totals[l1];
+			}
+			
+			//create the fourth column of matrix
+			for(unsigned int l1=0;l1<Y.size();l1++)
+			{
+				thread_local_features_ALT[l1][4] = counts[l1];
+			}
+			
+			std::vector<double> alt_model = glm(thread_local_features_ALT,Y,learn_rate,mx_iter);
+			
+			double alt_likelihood = 1.0;
+			for(int dat = 0; dat < thread_local_features_ALT.size(); ++dat){
+				std::vector<double> data(thread_local_features_ALT[0].size());
+				for(int j = 0; j<thread_local_features_ALT[0].size(); ++j){
+					data[j] = thread_local_features_ALT[dat][j];
+				}
+				double p = predict(alt_model, data);
+				
+				if( Y[dat] == 1){
+					alt_likelihood = alt_likelihood*p;
+				}
+				else{
+					alt_likelihood = alt_likelihood*(1.0-p); 
+				}
+			}
+
+			double null_likelihood = 1.0;
+			for(int dat = 0; dat < global_features_NULL.size(); ++dat){
+				std::vector<double> data(global_features_NULL[0].size());
+				for(int j = 0; j<global_features_NULL[0].size(); ++j){
+					data[j] = global_features_NULL[dat][j];
+				}
+				double p = predict(null_model, data);
+
+				if( ((int)Y[dat]) == 1){
+					null_likelihood = null_likelihood*p;
+				}
+				else{
+					null_likelihood = null_likelihood*(1.0-p); 
+				}
+			}
+
+			if((null_likelihood)==0 && (alt_likelihood)==0.0) {
+				null_likelihood = 0.001;
+				alt_likelihood = 1.0;
+			}
+
+			double likelihood_ratio = null_likelihood/alt_likelihood;
+			double log_likelihood_ratio = -2.0*(log(likelihood_ratio));
+
+			if(fabs(log_likelihood_ratio)<eps || log_likelihood_ratio<0.0){
+				log_likelihood_ratio = 0.0;
+			}
+
+			output[l] = alglib::chisquarecdistribution(1, log_likelihood_ratio);
+		}
+		
+		pthread_mutex_lock(&done_count_lock);
+		done_count++;
+		if(done_count==num_of_thread) {
+			sem_post(&all_done);
+		}
+		pthread_mutex_unlock(&done_count_lock);
+	}
+	
+	// delete ((thread_info *)arg);
 }
